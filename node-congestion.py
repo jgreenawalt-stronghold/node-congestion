b1f3414caeeade79281bb985fc4331a3087506e9
Jim Greenawalt <jgreenawalt@strongholddm.com>
2023-12-22 14:02:05 -0500

    fixed positional arguments with etl.extract_lmp

--- node-congestion.py
+++ node-congestion.py
@@ -27,10 +27,10 @@ async def main():
         etl = ETL()
         nodes = [Node(**obj) for obj in Node.node_data]
     
-        extract_tasks = [etl.extract_lmp(session, f"{etl.url_prefix}{node.pnode_id}{etl.get_pjm_params()}") for node in nodes]
+        extract_tasks = [etl.extract_lmp(f"{etl.url_prefix}{node.pnode_id}{etl.get_pjm_params()}", session) for node in nodes]
         extracted_prices = await asyncio.gather(*extract_tasks)
 
-        load_tasks = [etl.load_pi(session, node, data) for node, data in zip(nodes, extracted_prices)]
+        load_tasks = [etl.load_pi(node, data, session) for node, data in zip(nodes, extracted_prices)]
         loaded_price = await asyncio.gather(*load_tasks)
 
 if __name__ == "__main__":
